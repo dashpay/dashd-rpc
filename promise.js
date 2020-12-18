514dce92b4ac2c0d6fa47b2903a213c07e433fae
Cofresi <cofresi@dash.org>
2020-12-18 13:23:00 -0400

    chore: formatting

--- promise.js
+++ promise.js
@@ -1,17 +1,19 @@
+/* eslint-disable */
+
 RPCClient = require('./lib');
-Bluebird = require('bluebird')
+Bluebird = require('bluebird');
 
 class PromisifyModule {
-    constructor(options) {
-        const client = new RPCClient(options);
+  constructor(options) {
+    const client = new RPCClient(options);
 
-        for (let method in client.apiCalls) {
-            let promise = Bluebird.promisify(client[method]);
-            client[method] = promise;
-            client[method.toLowerCase()] = promise;
-        }
-
-        return client;
+    for (let method in client.apiCalls) {
+      let promise = Bluebird.promisify(client[method]);
+      client[method] = promise;
+      client[method.toLowerCase()] = promise;
     }
+
+    return client;
+  }
 }
-module.exports = PromisifyModule
+module.exports = PromisifyModule;

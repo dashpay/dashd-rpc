376725ab33fc32b83ed2e9781bffd346559e30a8
Cofresi <cofresi@dash.org>
2021-01-28 09:12:19 +0100

    feat: add quorum rpc commands (#31)
    * chore: update dependencies
    
    * fix: some easy linter errors
    
    * feat: add quorum rpc commands
    
    * chore: bump minor version
    
    * chore: formatting
    
    * more formatting
    
    * fix: adjust parameters
    
    * throw all Dashd rpc errors
    
    * fix: revert previous commit
    
    * detect more specific dashd errors
    
    * more specific error condition
    
    * remove more specific error condition
    
    * feat: add support for new rpc `quorum verify`
    
    * feat: add support for new rpc `verifyislock`

--- promise.js
+++ promise.js
@@ -1,17 +1,18 @@
-RPCClient = require('./lib');
-Bluebird = require('bluebird')
+const Bluebird = require('bluebird');
+const RPCClient = require('./lib');
 
 class PromisifyModule {
-    constructor(options) {
-        const client = new RPCClient(options);
+  constructor(options) {
+    const client = new RPCClient(options);
 
-        for (let method in client.apiCalls) {
-            let promise = Bluebird.promisify(client[method]);
-            client[method] = promise;
-            client[method.toLowerCase()] = promise;
-        }
-
-        return client;
+    // eslint-disable-next-line guard-for-in,no-restricted-syntax
+    for (const method in client.apiCalls) {
+      const promise = Bluebird.promisify(client[method]);
+      client[method] = promise;
+      client[method.toLowerCase()] = promise;
     }
+
+    return client;
+  }
 }
-module.exports = PromisifyModule
+module.exports = PromisifyModule;
